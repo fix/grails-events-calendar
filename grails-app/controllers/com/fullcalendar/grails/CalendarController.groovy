16c1ccaee55363bc29705babb7d4c2557d42496a
Francois-Xavier Thoorens <fx.thoorens@gmail.com>
2011-07-09 15:37:51 +0200

    fixed ical export not standard

--- grails-app/controllers/com/fullcalendar/grails/CalendarController.groovy
+++ grails-app/controllers/com/fullcalendar/grails/CalendarController.groovy
@@ -54,13 +54,12 @@ class CalendarController {
 	private String createIcal(long id){
 		def df=new java.text.SimpleDateFormat("yyyyMMdd'T'HHmmss")
 		Calendar c=Calendar.get(id)
-		def ical='''
-BEGIN:VCALENDAR
+		def ical='''BEGIN:VCALENDAR
 X-WR-CALNAME:'''+c.name+'''
 X-WR-CALDESC:GRAILS Plugin Calendar
 PRODID:-//Francois-Xavier Thoorens/NONSGML Bennu 0.1//EN
 VERSION:2.0
-	'''
+'''
 		c.events.each{
 			ical+="BEGIN:VEVENT\n"
 			ical+="UID:"+c.name+it.id+"@grails\n"
